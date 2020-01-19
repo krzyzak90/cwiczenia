3a6b01b8cd313ff3ad5ab7e0841dafa021b45f3b
Lukasz <krzyzak90@gmail.com>
2020-01-19 09:34:23 +0100

    'zajecia'

---
+++ src/main/java/org/lukasz/succes/Sratch99.java
@@ -0,0 +1,22 @@
+package org.lukasz.succes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sratch99 {
+//    public static void main(String[] args) throws IOException {
+//
+//        List<String> imiona = Files.readAllLines(Paths.get("imiona.txt"))
+//                .stream()
+//                .map(String::trim)
+//                .filter(String::isEmpty)
+//                .map((s) -> {
+//                    String[] split = s.split(", ");
+//                    return split[1] + " " + split[0];
+//                })
+//                .collect(Collectors.toList());
+//    }
+//}
---
+++ src/main/java/org/lukasz/succes/Watki2.java
@@ -0,0 +1,38 @@
+package org.lukasz.succes;
+
+public class Watki2 {
+    public static void main(String[] args) throws InterruptedException {
+        // 1. zadanie do wykonania przez wątek
+        Runnable zadanie = () -> {
+            try {
+                // każ wątkowi spać przez 5 s (5000 ms)
+                Thread.sleep(5000);
+                System.out.println("Wątek X: Cześć");
+                // śpij jeszcez raz
+                Thread.sleep(5000);
+                System.out.println("Wątek X: Kończę pracę");
+            } catch (InterruptedException e) {
+                // zignoruj wyjątek
+            }
+        };
+        // 2. utworzenie wątku
+        Thread wątekX = new Thread(zadanie);
+        // 3. uruchomienie wątku
+        wątekX.start();
+        // 4. sprawdź stan wątku
+        Thread.State stanWątkuX = wątekX.getState();
+        System.out.println("Stan wątku X: " + stanWątkuX);
+        // 5. zaczekanie na zakończenie pracy wątku
+        wątekX.join();
+        System.out.println("Program kończy działanie...");
+    }
+
+
+
+
+
+
+
+
+}
+}
